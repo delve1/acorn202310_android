8b891769878f142583ee7c1da6c5ba97a1743e8b
kimgura <oli999@naver.com>
2024-01-24 12:30:44 +0900

    step07 예제 완성함

--- step07fragment/src/main/java/com/example/step07fragment/MainActivity.java
+++ step07fragment/src/main/java/com/example/step07fragment/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.step07fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import com.example.step07fragment.databinding.ActivityMainBinding;
 
@@ -16,13 +18,20 @@ public class MainActivity extends AppCompatActivity {
         //setContentView(R.layout.activity_main);
         binding=ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        //액티비티의 화면에 전개된 프레그먼트의 참조값 얻어내기
+        FragmentManager fm=getSupportFragmentManager();
+        //MyFragment 의 참조값을 얻어와서
+        MyFragment f1=(MyFragment) fm.findFragmentById(R.id.fragment1);
+        MyFragment f2=(MyFragment) fm.findFragmentById(R.id.fragment2);
 
         //버튼에 클릭 리스너 등록
         binding.resetBtn.setOnClickListener(v->{
-
+            //MyFragment 가 가지고 있는 리셋 메소드를 호출하기
+            f1.reset();
+            f2.reset();
         });
         binding.moveBtn.setOnClickListener(v->{
-
+            startActivity(new Intent(this, SubActivity.class));
         });
     }
 }
