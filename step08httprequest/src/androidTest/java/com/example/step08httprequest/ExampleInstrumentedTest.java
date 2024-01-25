111b1306834a4be64a1e66587d98188a3e4a2522
kimgura <oli999@naver.com>
2024-01-25 12:24:55 +0900

    step07, step08 예제 추가함

--- step08httprequest/src/androidTest/java/com/example/step08httprequest/ExampleInstrumentedTest.java
+++ step08httprequest/src/androidTest/java/com/example/step08httprequest/ExampleInstrumentedTest.java
@@ -1,14 +1,14 @@
-package com.example.step07fragment;
-
-import static org.junit.Assert.assertEquals;
+package com.example.step08httprequest;
 
 import android.content.Context;
 
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.step07fragment", appContext.getPackageName());
+        assertEquals("com.example.step08httprequest", appContext.getPackageName());
     }
 }
